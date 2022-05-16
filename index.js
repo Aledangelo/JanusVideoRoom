fa08de04bcae8d538b3e6496ecd194a2dcbfb6c6
Aledangelo <alessandro.dangelo13.99@gmail.com>
2022-05-16 13:24:04 +0200

    Committo

--- index.js
+++ index.js
@@ -141,7 +141,6 @@ const server = https.createServer(options, app);
 
 server.listen(port, () => {
   console.log('[SERVER] Listening on https://' + ip.address() + ':' + port);
-  console.log('aaaaaaaprovaprova');
 });
 
 // app.post('/autenticazione', function(req, res) {
